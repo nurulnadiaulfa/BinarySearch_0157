1b56ee00b6f9aeb99028a5f1de6f2e6ab8f801d3
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-07 15:20:04 +0700

    menambahkan display masukkan elemen array

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -20,4 +20,13 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
         }
     }
+
+    cout << "\n=================================\n";
+    cout << "       Masukkan Element Array      \n";
+    cout << "\n=================================\n";
+    for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
 }
