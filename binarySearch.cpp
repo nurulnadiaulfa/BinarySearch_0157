24e6b12e61ecefc7f6f5969616fe578c0c812676
nurulnadiaulfa <nurulnadiaulf@gmail.com>
2025-05-07 15:49:44 +0700

    menambahkan int main

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -105,3 +105,11 @@ void binarySearch()
         cin >> ulang;
     } while (ulang == 'y' || ulang == 'Y');
 }
+
+int main()
+{
+    input();
+    bubbleSortArray();
+    display();
+    binarySearch();
+}
